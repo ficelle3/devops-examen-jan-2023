c285150ac17895edc1946275517a92888a117110
ficelle3 <appleoriginal911@gmail.com>
2023-01-20 10:34:17 +0100

    Goofy aah comments

--- src/services/validations.js
+++ src/services/validations.js
@@ -5,12 +5,110 @@ const isEmpty = (label) => !label || label.length === 0;
 exports.isEmpty = isEmpty;
 
 const isValidGamerTag = (gamerTag) => {
-	if(gamerTag.length < 8)return false;
-	const contains1Number = /[0-9]/g;
-	if(!gamerTag.match(contains1Number))return false;
-const contains1SpecialCharacter = /[\+\\\-\*\/\%\?\.\:\<\>\~\^\¨\€\$\[\]\(\)\{\}\#\@\&]/;
-	if(!gamerTag.match(contains1SpecialCharacter))return false;
+
+	if(gamerTag.length < 8)return false; //gamer tag isn't valid if it contains less than 8 characters
+
+	const contains1Number = /[0-9]/g; //match any number between 0 and 9
+	if(!gamerTag.match(contains1Number))return false; // if there is at least one match, gamer tag is valid
+
+	const contains1SpecialCharacter = /[\+\\\-\*\/\%\?\.\:\<\>\~\^\¨\€\$\[\]\(\)\{\}\#\@\&]/; //match these special characters : +\-*/%?.:<>~^¨€$[]{}()#@&
+	if(!gamerTag.match(contains1SpecialCharacter))return false; // if there is at least one match, gamer tag is valid
+
 	return true;
 };
 
 exports.isValidGamerTag = isValidGamerTag;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+⬛⬛⬛⬛⬛⬛⬛⬜⬛⬛⬜⬜⬜⬜⬜⬜⬛⬜⬛⬛⬛⬛⬛⬛⬛
+⬛⬜⬜⬜⬜⬜⬛⬜⬜⬜⬜⬜⬜⬜⬜⬜⬛⬜⬛⬜⬜⬜⬜⬜⬛
+⬛⬜⬛⬛⬛⬜⬛⬜⬛⬜⬜⬛⬛⬛⬜⬛⬜⬜⬛⬜⬛⬛⬛⬜⬛
+⬛⬜⬛⬛⬛⬜⬛⬜⬛⬛⬜⬛⬛⬜⬛⬜⬜⬜⬛⬜⬛⬛⬛⬜⬛
+⬛⬜⬛⬛⬛⬜⬛⬜⬛⬛⬛⬛⬜⬜⬜⬛⬜⬜⬛⬜⬛⬛⬛⬜⬛
+⬛⬜⬜⬜⬜⬜⬛⬜⬜⬛⬜⬜⬛⬛⬜⬜⬜⬜⬛⬜⬜⬜⬜⬜⬛
+⬛⬛⬛⬛⬛⬛⬛⬜⬛⬜⬛⬜⬛⬜⬛⬜⬛⬜⬛⬛⬛⬛⬛⬛⬛
+⬜⬜⬜⬜⬜⬜⬜⬜⬜⬛⬜⬛⬜⬜⬜⬜⬛⬜⬜⬜⬜⬜⬜⬜⬜
+⬛⬛⬛⬛⬜⬜⬛⬜⬛⬜⬜⬜⬛⬜⬛⬜⬛⬛⬜⬜⬛⬛⬛⬜⬛
+⬜⬜⬜⬜⬛⬜⬜⬛⬜⬛⬜⬜⬜⬜⬛⬜⬛⬜⬜⬛⬜⬜⬜⬛⬜
+⬜⬛⬛⬛⬛⬜⬛⬛⬜⬜⬜⬛⬛⬛⬜⬜⬜⬜⬛⬛⬛⬜⬜⬜⬜
+⬜⬛⬛⬛⬛⬜⬜⬛⬜⬜⬜⬛⬜⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬜⬜
+⬜⬜⬛⬜⬜⬛⬛⬜⬜⬛⬜⬜⬛⬛⬜⬛⬜⬛⬛⬛⬛⬜⬛⬛⬛
+⬜⬜⬜⬜⬛⬛⬜⬜⬛⬛⬛⬜⬛⬜⬜⬛⬛⬜⬛⬛⬛⬜⬜⬜⬛
+⬜⬛⬜⬛⬜⬜⬛⬛⬜⬜⬜⬜⬜⬛⬛⬜⬛⬛⬜⬜⬛⬜⬛⬛⬜
+⬛⬜⬛⬛⬛⬜⬜⬜⬛⬛⬜⬜⬛⬜⬜⬜⬛⬛⬛⬛⬛⬜⬜⬜⬛
+⬜⬜⬜⬛⬛⬛⬛⬜⬜⬛⬜⬜⬛⬛⬜⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+⬜⬜⬜⬜⬜⬜⬜⬜⬛⬛⬜⬜⬛⬛⬛⬜⬛⬜⬜⬜⬛⬜⬛⬜⬛
+⬛⬛⬛⬛⬛⬛⬛⬜⬜⬜⬜⬜⬜⬛⬛⬜⬛⬜⬛⬜⬛⬜⬛⬛⬛
+⬛⬜⬜⬜⬜⬜⬛⬜⬜⬜⬛⬜⬛⬜⬜⬜⬛⬜⬜⬜⬛⬜⬜⬛⬜
+⬛⬜⬛⬛⬛⬜⬛⬜⬜⬛⬛⬜⬜⬜⬜⬜⬛⬛⬛⬛⬛⬛⬜⬛⬛
+⬛⬜⬛⬛⬛⬜⬛⬜⬛⬛⬜⬛⬜⬛⬛⬜⬜⬛⬛⬜⬛⬛⬛⬛⬛
+⬛⬜⬛⬛⬛⬜⬛⬜⬛⬜⬜⬛⬛⬜⬛⬛⬜⬜⬛⬜⬛⬜⬛⬛⬜
+⬛⬜⬜⬜⬜⬜⬛⬜⬛⬜⬜⬛⬛⬛⬛⬛⬜⬛⬜⬜⬛⬜⬛⬜⬜
+⬛⬛⬛⬛⬛⬛⬛⬜⬛⬛⬛⬜⬜⬜⬛⬜⬜⬜⬛⬛⬛⬛⬛⬛⬛
+*/
