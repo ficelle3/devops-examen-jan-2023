02c5e80f80a496ca1504c87610e1ff325b5c9657
ficelle3 <appleoriginal911@gmail.com>
2023-01-20 09:40:27 +0100

    Added tests

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -17,5 +17,24 @@ describe("validations tests suites - isEmpty", () => {
     });
 });
 
+describe("validation tests suites - isValidGamerTag", () => {
+	test("should return false if the length is less than 8", () => {
+		const result = isValidGamerTag("a1€");
+		expect(result).toBe(false);
+	});
+	test("should return false if the gamer tag doesn't contain at least one special character", () => {
+		const result = isValidGamerTag("0123456789ABCDEF");
+		expect(result).toBe(false);
+	});
+	test("should return false if the gamer tag doesn't contain at least one number", () => {
+		const result = isValidGamerTag("a-b-c-d-e-f");
+		expect(result).toBe(false);
+	});
+	test("should return true if the gamer tag is valid", () => {
+		const result = isValidGamerTag("Jean-Kevin-du-83");
+		expect(result).toBe(false);
+	});
+
+});
 
 // TODO: Create tests suite for validation function
