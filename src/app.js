b4070c35afe22bd32d44094b7b18b37f1477e699
ficelle3 <appleoriginal911@gmail.com>
2023-01-20 10:18:49 +0100

    Added gamertag check to check button

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 /* istanbul ignore file */
 try {
-    const { isEmpty } = require("./services/validations");
+    const { isEmpty, isValidGamerTag } = require("./services/validations");
     const gamerTagInput = document.getElementById("gamerTagInput");
     const checkButton = document.getElementById("gamerTagCheckButton");
     const feedbackMessage = document.getElementById("gamerTagFeedback");
@@ -8,15 +8,22 @@ try {
     var gamerTagValue;
     var feedbackMessageText;
 
+
     gamerTagInput.addEventListener("change", (event) => {
         gamerTagValue = event.target.value;
     });
 
     checkButton.addEventListener("click", () => {
         // TODO: Add the logic to display the correct feedback message (error and validation)
-        feedbackMessageText = isEmpty(gamerTagValue)
-            ? "Gamer tag cannot be empty"
-            : "Gamer tag is valid";
+	if(isEmpty(gamerTagValue)){
+		feedbackMessageText = "Gamer tag cannot be empty"; 
+	}else{
+		if(!isValidGamerTag(gamerTagValue)){
+			feedbackMessageText = "Gamer tag must contain at least 8 characters, including one number and one special character";
+		}else{
+			feedbackMessageText = "Gamer tag is valid";
+		}
+	}
         feedbackMessage.textContent = feedbackMessageText;
     });
 } catch (err) {
